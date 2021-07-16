8739617b2fb2399ba8c66d0b40e01bf4a1e55475
kchasepdx <kchasepdx@gmail.com>
2021-07-15 20:14:55 -0700

    update showfinder page

--- src/ShowFinder.jsx
+++ src/ShowFinder.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Nav from "./Nav";
+import ReactRouterLogo from "./Images/react-router-logo.png";
+import ReactLogo from "./Images/react-logo.png";
+import JSLogo from "./Images/JSlogo.png";
+import BSLogo from "./Images/Bootstrap-logo.png";
 
 function ShowFinder() {
   return (
     <div className="container-fluid">
       <Nav />
       <div id="showfinder-div">
-        <h1>Show Finder</h1>
+        <h1 className="proj-headers">Show Finder</h1>
+        <div className="container-fluid logo-container">
+          <img class="logo-img" id="bs-logo" src={BSLogo} />
+          <img class="logo-img" id="router-logo" src={ReactRouterLogo} />
+          <img class="logo-img" id="react-logo" src={ReactLogo} />
+          <img class="logo-img" id="js-logo" src={JSLogo} />
+        </div>
         <a href="https://kchasepdx.github.io/showfinder">
-          <button className="btn-primary">Visit</button>
+          <button className="btn-primary" id="sf-visit">
+            Visit
+          </button>
         </a>
-        <ul>
-          <li>Bootstrap</li>
-          <li>CSS</li>
-          <li>React</li>
-          <li>HTML</li>
-          <li>Javascript</li>
-          <li>Browser Router</li>
-          <li>API</li>
-        </ul>
       </div>
     </div>
   );
