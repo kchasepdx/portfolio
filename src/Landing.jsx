dce650fbebbe0b6b74d60f2cb14059d2551be268
kchasepdx <kchasepdx@gmail.com>
2021-07-19 09:48:12 -0700

    remove useEffect

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import screenshot from "./Images/code-screenshot.png";
 import me from "./Images/kristen-circle.png";
