3203acfaa3cc58621a0028619c57dc80962a5fe1
kchasepdx <kchasepdx@gmail.com>
2024-09-09 13:52:33 -0700

    copyright year dynamic

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  console.log("currentYear", currentYear);
   return (
     <div id="footer-div">
-      <footer id="footer">© Kristen Chase 2023 | All Rights Reserved</footer>
+      <footer id="footer">
+        © Kristen Chase {currentYear} | All Rights Reserved
+      </footer>
     </div>
   );
 }
